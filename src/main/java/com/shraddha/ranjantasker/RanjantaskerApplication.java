4f0db2a4122348fd1df4b689dd852744081942d1
Shraddha Ranjan <ranjanshraddha@yahoo.co.uk>
2024-11-19 18:00:12 +0000

    Setup

---
+++ src/main/java/com/shraddha/ranjantasker/RanjantaskerApplication.java
@@ -0,0 +1,13 @@
+package com.shraddha.ranjantasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanjantaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanjantaskerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shraddha/ranjantasker/RanjantaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shraddha.ranjantasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanjantaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
