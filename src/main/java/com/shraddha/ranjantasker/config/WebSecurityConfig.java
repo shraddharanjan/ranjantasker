b29965598701e3884f882b31aaec579fcd0d06a7
Shraddha Ranjan <ranjanshraddha@yahoo.co.uk>
2024-11-20 17:13:31 +0000

    Custom authentication success handler

--- src/main/java/com/shraddha/ranjantasker/config/WebSecurityConfig.java
+++ src/main/java/com/shraddha/ranjantasker/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     private final String[] publicUrl = {"/",
             "/global-search/**",
@@ -33,8 +35,9 @@ public class WebSecurityConfig {
     };
 
     @Autowired
-    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService, CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
         this.customUserDetailsService = customUserDetailsService;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
     }
 
     @Bean
@@ -44,6 +47,13 @@ public class WebSecurityConfig {
             auth.requestMatchers(publicUrl).permitAll();
             auth.anyRequest().authenticated();
         });
+        http.formLogin(form -> form.loginPage("/login").permitAll().successHandler(customAuthenticationSuccessHandler))
+                .logout(logout -> {
+                    logout.logoutUrl("/logout");
+                    logout.logoutSuccessUrl("/");
+                }).cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
                 return http.build();
     }
 
--- src/main/java/com/shraddha/ranjantasker/services/UsersService.java
+++ src/main/java/com/shraddha/ranjantasker/services/UsersService.java
@@ -7,6 +7,7 @@ import com.shraddha.ranjantasker.repository.RequesterAccountRepository;
 import com.shraddha.ranjantasker.repository.TaskSeekerAccountRepository;
 import com.shraddha.ranjantasker.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,17 +19,20 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final TaskSeekerAccountRepository taskSeekerAccountRepository;
     private final RequesterAccountRepository requesterAccountRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository, TaskSeekerAccountRepository taskSeekerAccountRepository, RequesterAccountRepository requesterAccountRepository) {
+    public UsersService(UsersRepository usersRepository, TaskSeekerAccountRepository taskSeekerAccountRepository, RequesterAccountRepository requesterAccountRepository, PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.taskSeekerAccountRepository = taskSeekerAccountRepository;
         this.requesterAccountRepository = requesterAccountRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users){
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUser = usersRepository.save(users);
         int userCategoryId = users.getUserCategoryId().getUserCategoryId();
         if (userCategoryId == 1){
