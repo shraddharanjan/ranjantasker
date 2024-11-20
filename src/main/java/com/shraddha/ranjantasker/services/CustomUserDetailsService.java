31ea65bfabe1714c545c3129485c324bcda51d4b
Shraddha Ranjan <ranjanshraddha@yahoo.co.uk>
2024-11-20 16:57:22 +0000

    Develop custom user authentication and authorization

---
+++ src/main/java/com/shraddha/ranjantasker/services/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.shraddha.ranjantasker.services;
+
+import com.shraddha.ranjantasker.entity.Users;
+import com.shraddha.ranjantasker.repository.UsersRepository;
+import com.shraddha.ranjantasker.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not find user"));
+        return new CustomUserDetails(user);
+    }
+}
---
+++ src/main/java/com/shraddha/ranjantasker/util/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package com.shraddha.ranjantasker.util;
+
+import com.shraddha.ranjantasker.entity.Users;
+import com.shraddha.ranjantasker.entity.UsersCategory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Users user;
+
+    public CustomUserDetails(Users user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UsersCategory usersCategory = user.getUserCategoryId();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(usersCategory.getUserCategoryName()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
