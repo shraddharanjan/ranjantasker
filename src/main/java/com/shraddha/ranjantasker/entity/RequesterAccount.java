dc8f1d19ebc345867aed99f3ea51d468fb8487b1
Shraddha Ranjan <ranjanshraddha@yahoo.co.uk>
2024-11-20 16:00:17 +0000

    Create profiles for requesters and task seekers

---
+++ src/main/java/com/shraddha/ranjantasker/entity/RequesterAccount.java
@@ -0,0 +1,106 @@
+package com.shraddha.ranjantasker.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "requester_account")
+public class RequesterAccount {
+
+    @Id
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    @MapsId
+    private Users userId;
+
+    private String firstName;
+    private String surname;
+    private String city;
+    private String country;
+
+    @Column(nullable = true, length = 64)
+    private String profilePhoto;
+
+    public RequesterAccount() {
+    }
+
+    public RequesterAccount(int userAccountId, Users userId, String firstName, String surname, String city, String country, String profilePhoto) {
+        this.userAccountId = userAccountId;
+        this.userId = userId;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.city = city;
+        this.country = country;
+        this.profilePhoto = profilePhoto;
+    }
+
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    @Override
+    public String toString() {
+        return "RequesterAccount{" +
+                "userAccountId=" + userAccountId +
+                ", userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", surname='" + surname + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/shraddha/ranjantasker/entity/TaskSeekerAccount.java
@@ -0,0 +1,132 @@
+package com.shraddha.ranjantasker.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "task_seeker_account")
+public class TaskSeekerAccount {
+
+    @Id
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    @MapsId
+    private Users userId;
+
+    private String firstName;
+    private String surname;
+    private String city;
+    private String country;
+    private String employmentType;
+    private String cv;
+
+    @Column(nullable = true, length = 64)
+    private String profilePhoto;
+
+    public TaskSeekerAccount() {
+    }
+
+    public TaskSeekerAccount(Users userId) {
+        this.userId = userId;
+    }
+
+    public TaskSeekerAccount(int userAccountId, Users userId, String firstName, String surname, String city, String country, String employmentType, String cv, String profilePhoto) {
+        this.userAccountId = userAccountId;
+        this.userId = userId;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.city = city;
+        this.country = country;
+        this.employmentType = employmentType;
+        this.cv = cv;
+        this.profilePhoto = profilePhoto;
+    }
+
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmploymentType() {
+        return employmentType;
+    }
+
+    public void setEmploymentType(String employmentType) {
+        this.employmentType = employmentType;
+    }
+
+    public String getCv() {
+        return cv;
+    }
+
+    public void setCv(String cv) {
+        this.cv = cv;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskSeekerAccount{" +
+                "userAccountId=" + userAccountId +
+                ", userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", surname='" + surname + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", employmentType='" + employmentType + '\'' +
+                ", cv='" + cv + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
+                '}';
+    }
+}
