af758f153ab93176a509603d9e6ee8bf50725b0c
Shraddha Ranjan <ranjanshraddha@yahoo.co.uk>
2024-11-22 22:28:06 +0000

    Create taskSeekerAccount- controller and service

--- src/main/java/com/shraddha/ranjantasker/controller/TaskSeekerAccountController.java
+++ src/main/java/com/shraddha/ranjantasker/controller/TaskSeekerAccountController.java
@@ -12,7 +12,12 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/task-seeker-account")
@@ -33,7 +38,26 @@ public class TaskSeekerAccountController {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
             if (!(authentication instanceof AnonymousAuthenticationToken)) {
                 Users user = usersRepository.findByEmail(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException("User mot found"));
+                Optional<TaskSeekerAccount> seekerAccount = taskSeekerAccountService.getOne(user.getUserId());
+                if (seekerAccount.isPresent()) {
+                    taskSeekerAccount = seekerAccount.get();
+                }
+                model.addAttribute("account", taskSeekerAccount);
             }
             return "task-seeker-account";
+        }
+
+        @PostMapping("/addNew")
+    public String addNew(TaskSeekerAccount taskSeekerAccount, @RequestParam("image")MultipartFile image, @RequestParam("pdf") MultipartFile pdf, Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            Users user = usersRepository.findByEmail(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            taskSeekerAccount.setUserId(user);
+            taskSeekerAccount.setUserAccountId(user.getUserId());
+        }
+        model.addAttribute("account", taskSeekerAccount);
+        return  "redirect:/dashboard/"; 
+
         }
 }
--- src/main/java/com/shraddha/ranjantasker/services/TaskSeekerAccountService.java
+++ src/main/java/com/shraddha/ranjantasker/services/TaskSeekerAccountService.java
@@ -1,7 +1,21 @@
 package com.shraddha.ranjantasker.services;
 
+import com.shraddha.ranjantasker.entity.TaskSeekerAccount;
+import com.shraddha.ranjantasker.repository.TaskSeekerAccountRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TaskSeekerAccountService {
+
+    private final TaskSeekerAccountRepository taskSeekerAccountRepository;
+
+    public TaskSeekerAccountService(TaskSeekerAccountRepository taskSeekerAccountRepository) {
+        this.taskSeekerAccountRepository = taskSeekerAccountRepository;
+    }
+
+    public Optional<TaskSeekerAccount> getOne(Integer id) {
+        return taskSeekerAccountRepository.findById(id);
+    }
 }
